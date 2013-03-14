bcee5b5d136ae3c91a202478b0e83f58edda541d
Christopher Baker <me@christopherbaker.net>
2013-03-14 12:47:40 -0500

    Example upload server.

---
+++ example-server-websocket-videoplayer/src/KibioMasker.h
@@ -0,0 +1,14 @@
+//
+//  KibioMasker.h
+//  example-server-websocket-videoplayer
+//
+//  Created by Christopher P. Baker on 3/12/13.
+//
+//
+
+#ifndef __example_server_websocket_videoplayer__KibioMasker__
+#define __example_server_websocket_videoplayer__KibioMasker__
+
+#include <iostream>
+
+#endif /* defined(__example_server_websocket_videoplayer__KibioMasker__) */
