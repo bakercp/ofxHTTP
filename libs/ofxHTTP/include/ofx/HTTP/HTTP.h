5851529620cb4845521794f2271d1564a453f1cb
Christopher Baker <me@christopherbaker.net>
2014-03-17 10:25:11 -0500

    Doc formatting.

--- libs/ofxHTTP/include/ofx/HTTP/HTTP.h
+++ libs/ofxHTTP/include/ofx/HTTP/HTTP.h
@@ -26,15 +26,16 @@
 #pragma once
 
 
+/// \namespace ofx
+/// \brief openFrameworks Extended
+/// \sa http://openframeworks.cc
+/// \sa http://ofxaddons.com
 namespace ofx
-    /// \namespace ofx
-    /// \brief openFrameworks Extended
-    /// \sa http://openframeworks.cc
-    /// \sa http://ofxaddons.com
 {
+
+/// \namespace HTTP
+/// \brief HTTP is a package for HTTP Client and Server activities.
 namespace HTTP
-    // \namespace HTTP
-    // \brief HTTP is a package for HTTP Client and Server activities.
 {
 
 
