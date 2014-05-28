dc658921c7fbab24e73f6d3b82418d3eda6e6268
Christopher Baker <me@christopherbaker.net>
2014-05-27 19:27:20 -0500

    Updated addon_config.mk.

---
+++ addon_config.mk
@@ -0,0 +1,10 @@
+meta:
+	ADDON_NAME = ofxHTTP
+	ADDON_DESCRIPTION = An HTTP addon for client and HTTP custom clients and servers.
+	ADDON_AUTHOR = bakercp
+	ADDON_TAGS = "http" "web" "client" "server"
+	ADDON_URL = http://github.com/bakercp/ofxHTTP
+common:
+	# dependencies with other addons, a list of them separated by spaces 
+	# or use += in several lines
+	ADDON_DEPENDENCIES = ofxSSLManager ofxIO ofxMediaType ofxTaskQueue
