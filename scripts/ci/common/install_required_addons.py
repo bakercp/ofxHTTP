fb3f4d62d1e815dbede140da63a5e4c874867626
Christopher Baker <me@christopherbaker.net>
2017-01-25 10:12:17 -0600

    Continued CI testing.

--- scripts/ci/common/install_required_addons.py
+++ scripts/ci/common/install_required_addons.py
@@ -1,33 +1,50 @@
 """This thing ... """
 
-import subprocess
 import argparse
+import os
+import subprocess
 from pathlib import Path
 
 parser = argparse.ArgumentParser()
 
+# parser.add_argument("--addon_path", help="The path of the addon.")
+parser.add_argument("--of_root", help="The OF_ROOT directory.")
+# parser.add_argument("--of_addons", help="The OF_ADDONS directory.")
 parser.add_argument("--username", help="The default github username.")
 parser.add_argument("--branch", help="The default github branch.")
 parser.add_argument("--depth", help="The default github clone depth.")
 
 args = parser.parse_args()
 
-GH_USERNAME = args.username if args.username else 'bakercp'
-GH_BRANCH = args.branch if args.branch else 'master'
-GH_DEPTH = args.depth if args.depth else 1
 
 # These paths assume that this file is located in
 #   OF_ROOT/addons/ADDON_NAME/ci/common/install_dependencies.py
 
 ADDON_PATH = Path('../../..')
 ADDON_NAME = ADDON_PATH.resolve().name
-OF_ADDONS = Path('../../../..')
-OF_ROOT = Path('../../../../..')
+
+if args.of_root:
+    OF_ROOT = Path(args.of_root).expanduser()
+elif os.getenv('OF_ROOT'):
+    OF_ROOT = Path(os.getenv('OF_ROOT')).expanduser()
+else:
+    OF_ROOT = Path('../../../../..').resolve()
+
+OF_ADDONS = OF_ROOT / 'addons'
+
+GH_USERNAME = args.username if args.username else 'bakercp'
+GH_BRANCH = args.branch if args.branch else 'master'
+GH_DEPTH = args.depth if args.depth else 1
 
 print(ADDON_PATH)
 print(ADDON_NAME)
-print(OF_ADDONS)
 print(OF_ROOT)
+print(OF_ADDONS.resolve())
+
+# print(ADDON_PATH)
+# print(ADDON_NAME)
+# print(OF_ADDONS)
+# print(OF_ROOT)
 
 REQUIRED_ADDONS = set([])
 
@@ -51,9 +68,9 @@ print(REQUIRED_ADDONS)
 # Remove this addon from the list.
 REQUIRED_ADDONS.remove(ADDON_NAME)
 
-for addon in REQUIRED_ADDONS:
-    addon_path = OF_ADDONS.resolve() / addon
-    if not addon_path.exists():
+for required_addon in REQUIRED_ADDONS:
+    required_addon_path = OF_ADDONS.resolve() / addon
+    if not required_addon_path.exists():
         repository = "https://github.com/" + GH_USERNAME + "/" + addon + ".git"
-        subprocess.call(["git", "clone", repository, str(addon_path)])
+        subprocess.call(["git", "clone", repository, str(required_addon_path)])
 
