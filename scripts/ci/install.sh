8b59525680867dd7cc0a5972978647070e76c10f
Christopher Baker <me@christopherbaker.net>
2017-05-08 17:08:34 -0500

    Add-on dependencies update.

--- scripts/ci/install.sh
+++ scripts/ci/install.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
-OF_ROOT=$( cd "$(dirname "$0")/../../../.." ; pwd -P )
+THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if ! [ -z ${OF_ROOT} ]; then
+  OF_ROOT=$( cd "$(dirname "$0")/../../../.." ; pwd -P )
+fi
+
+if ! [ -z ${ADDON_NAME} ]; then
+  ADDON_NAME=$(basename $( cd "$(dirname "$0")/../../.." ; pwd -P ))
+fi
 
 # Default addon github info.
 GH_USERNAME='bakercp'
@@ -12,7 +20,17 @@ GH_DEPTH=1
 REQUIRED_ADDONS=()
 
 # Extract ADDON_DEPENDENCIES from addon_config.mk file.
-if [ -f ${OF_ROOT}/addons/${OF_ADDON_NAME}/addon_config.mk ]; then
+ADDON_CONFIG_MK=$OF_ROOT/addons/$ADDON_NAME/addon_config.mk
+
+echo "Running ${THIS_DIR}/install.sh"
+echo "---------------------------------"
+echo "        OF_ROOT: ${OF_ROOT}"
+echo "     ADDON_NAME: ${ADDON_NAME}"
+echo "ADDON_CONFIG_MK: ${ADDON_NAME}"
+echo ""
+
+echo "Examining ${ADDON_CONFIG_MK} ..."
+if [ -f $ADDON_CONFIG_MK ]; then
   while read line; do
     if [[ $line == ADDON_DEPENDENCIES* ]] ;
     then
@@ -22,13 +40,17 @@ if [ -f ${OF_ROOT}/addons/${OF_ADDON_NAME}/addon_config.mk ]; then
           REQUIRED_ADDONS+=($i)
       done
     fi
-  done < ${OF_ROOT}/addons/${OF_ADDON_NAME}/addon_config.mk
+  done < $ADDON_CONFIG_MK
+else
+  echo "No ${ADDON_CONFIG_MK} found, skipping."
 fi
 
+echo "Gathering addons from addons.make files ..."
 # Gather addons from all examples.
-for addons_make in ${OF_ROOT}/addons/${OF_ADDON_NAME}/example*/addons.make; do
+for addons_make in $OF_ROOT/addons/$ADDON_NAME/example*/addons.make; do
+  echo "Examining ${addons_make} ..."
   while read addon; do
-    if [ ${addon} != ${OF_ADDON_NAME} ] ;
+    if [ ${addon} != ${ADDON_NAME} ] ;
     then
       REQUIRED_ADDONS+=($addon)
     fi
@@ -36,9 +58,15 @@ for addons_make in ${OF_ROOT}/addons/${OF_ADDON_NAME}/example*/addons.make; do
 done
 
 # We aren't de-duplicating array to keep it pure bash.
+echo "Cloning addons ..."
 for addon in "${REQUIRED_ADDONS[@]}"
 do
   if [ ! -d ${OF_ROOT}/addons/${addon} ]; then
+    echo "Cloning addon: ${addon}."
     git clone --depth=$GH_DEPTH https://github.com/$GH_USERNAME/$addon.git ${OF_ROOT}/addons/${addon}
+  else
+    echo "Addon: ${addon} already exists, skipping."
   fi
 done
+
+echo "Finished installing addon dependencies."
